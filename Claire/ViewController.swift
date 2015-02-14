667400f11b39f571a325da1b314ff34d122d9736
yazo <yazoglr@gmail.com>
2015-02-15 00:36:51 +0200

    Initial Commit

---
+++ Claire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Claire
+//
+//  Created by Yağızhan Güler on 15/02/15.
+//  Copyright (c) 2015 Yağızhan Güler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
